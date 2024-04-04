4e7ba7f2cf693f98fbbf805d940c1f9dcbc7d0d2
Mirko Lenz <mirko@mirkolenz.com>
2024-04-04 11:27:54 +0200

    update nix flake

--- flake.nix
+++ flake.nix
@@ -4,78 +4,81 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
   };
-  outputs = inputs @ {
-    flake-parts,
-    nixpkgs,
-    systems,
-    ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs =
+    inputs@{
+      flake-parts,
+      nixpkgs,
+      systems,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import systems;
-      perSystem = {
-        pkgs,
-        lib,
-        system,
-        self',
-        ...
-      }: {
-        packages = {
-          bumpArgServices = pkgs.writeShellApplication {
-            name = "bump-arg-services";
-            text = ''
-              if [ "$#" -ne 1 ]; then
-                echo "Usage: $0 VERSION" >&2
-                exit 1
-              fi
-
-              # remove v prefix
-              version="''${1#v}"
-              versionTag="v$version"
-
-              # split into array
-              IFS="." read -ra versionArray <<< "$version"
-
-              if [ "''${#versionArray[@]}" -ne 3 ]; then
-                echo "Version must be in format x.y.z" >&2
-                exit 1
-              fi
-
-              minorVersion="''${versionArray[1]}"
-              patchVersion="''${versionArray[2]}"
-
-              commitPrefix="fix"
-
-              if [ "$patchVersion" -eq 0 ]; then
-                commitPrefix="feat"
-              fi
-              if [ "$patchVersion" -eq 0 ] && [ "$minorVersion" -eq 0 ]; then
-                commitPrefix="feat!"
-              fi
-
-              # update deps in buf.yaml
-              ${lib.getExe pkgs.sd} -- '(buf.build/recap/arg-services):v.*' "\$1:$versionTag" */buf.yaml
-
-              # update buf.lock
-              ${lib.getExe pkgs.mu-repo} sh ${lib.getExe pkgs.buf} mod update
-
-              commitMessage="$commitPrefix: bump arg-services to $versionTag"
-
-              # https://stackoverflow.com/a/1885534
-              read -p "Commit changes with message '$commitMessage' (y/n)? " -r shouldCommit
-              if [ "$shouldCommit" = "y" ]; then
-                ${lib.getExe pkgs.mu-repo} ac "$commitMessage"
-
-                read -p "Push commit (y/n)? " -r shouldPush
-                if [ "$shouldPush" = "y" ]; then
-                    ${lib.getExe pkgs.mu-repo} p
+      perSystem =
+        {
+          pkgs,
+          lib,
+          system,
+          self',
+          ...
+        }:
+        {
+          packages = {
+            bump = pkgs.writeShellApplication {
+              name = "bump-arg-services";
+              text = ''
+                if [ "$#" -ne 1 ]; then
+                  echo "Usage: $0 VERSION" >&2
+                  exit 1
                 fi
-              fi
-            '';
+
+                # remove v prefix
+                version="''${1#v}"
+                versionTag="v$version"
+
+                # split into array
+                IFS="." read -ra versionArray <<< "$version"
+
+                if [ "''${#versionArray[@]}" -ne 3 ]; then
+                  echo "Version must be in format x.y.z" >&2
+                  exit 1
+                fi
+
+                minorVersion="''${versionArray[1]}"
+                patchVersion="''${versionArray[2]}"
+
+                commitPrefix="fix"
+
+                if [ "$patchVersion" -eq 0 ]; then
+                  commitPrefix="feat"
+                fi
+                if [ "$patchVersion" -eq 0 ] && [ "$minorVersion" -eq 0 ]; then
+                  commitPrefix="feat!"
+                fi
+
+                # update deps in buf.yaml
+                ${lib.getExe pkgs.sd} -- '(buf.build/recap/arg-services):v.*' "\$1:$versionTag" */buf.yaml
+
+                # update buf.lock
+                ${lib.getExe pkgs.mu-repo} sh ${lib.getExe pkgs.buf} mod update
+
+                commitMessage="$commitPrefix: bump arg-services to $versionTag"
+
+                # https://stackoverflow.com/a/1885534
+                read -p "Commit changes with message '$commitMessage' (y/n)? " -r shouldCommit
+                if [ "$shouldCommit" = "y" ]; then
+                  ${lib.getExe pkgs.mu-repo} ac "$commitMessage"
+
+                  read -p "Push commit (y/n)? " -r shouldPush
+                  if [ "$shouldPush" = "y" ]; then
+                      ${lib.getExe pkgs.mu-repo} p
+                  fi
+                fi
+              '';
+            };
+          };
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [ mu-repo ] ++ lib.attrValues self'.packages;
           };
         };
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [mu-repo] ++ lib.attrValues self'.packages;
-        };
-      };
     };
 }
